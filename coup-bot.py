d50003114e0506eeb76f4f3e86938b8d3250b387
corissau <corissau@gmail.com>
2022-09-06 17:23:15 +1000

    discard logic

--- coup-bot.py
+++ coup-bot.py
@@ -1,3 +1,4 @@
+from operator import contains
 from submission_helper.bot_battle import BotBattle
 from submission_helper.state import *
 from submission_helper.enums import *
@@ -17,7 +18,8 @@ bot_battle = BotBattle()
 
 our_bot = Player()
 
-
+# gets the closes player that is left alive in turn order
+# clockwise(?) and returns their index
 def get_next_alive_player():
     next_alive = (game_info.player_id + 1) % 5
     while game_info.players_cards_num[next_alive] == 0:
@@ -26,6 +28,9 @@ def get_next_alive_player():
     return next_alive
 
 
+# Calls the appropriate function for the action requested
+# of us. Could be a primary actoin, challenge, counter, 
+# challenge response, or discard a card
 def move_controller(requested_move: RequestedMove):
     if requested_move == RequestedMove.PrimaryAction:
         primary_action_handler()
@@ -45,7 +50,7 @@ def move_controller(requested_move: RequestedMove):
     else:
         return Exception(f'Unknown requested move: {requested_move}')
 
-
+# the main "play" of the game when it is our turn
 def primary_action_handler():
     if game_info.balances[game_info.player_id] >= 10:
         target_player_id = get_next_alive_player()
@@ -53,23 +58,40 @@ def primary_action_handler():
     else:
         bot_battle.play_primary_action(PrimaryAction.Income)
 
-
+# TODO: add logic here for when we want to counter
 def counter_action_handler():
     bot_battle.play_counter_action(CounterAction.NoCounterAction)
 
-
+# TODO: add logic here for when we want to call someone's bluff
 def challenge_action_handler():
     bot_battle.play_challenge_action(ChallengeAction.NoChallenge)
 
-
+# TODO: is this the part that asks us if we're lying or not?
 def challenge_response_handler():
     bot_battle.play_challenge_response(0)
 
-
+# TODO: discards the card of least value to prepare for endgame
+# strategy in the following order
+# Captain > Assassin > Contessa > Duke > Ambassador
 def discard_choice_handler():
-    bot_battle.play_discard_choice(0)
+    if contains(game_info.own_cards, Character.Ambassador):
+        # how do we know which index the Ambassador card is in?
+        bot_battle.play_discard_choice(1) # discard Ambassador instead of Assassin or Captain
+    
+    elif contains(game_info.own_cards, Character.Duke):
+        bot_battle.play_discard_choice(1)# discard Duke instead of Assassin or Captain
 
+    elif contains(game_info.own_cards, Character.Contessa):
+        bot_battle.play_discard_choice(1) # discard Contessa instead of Assassin or Captain
 
+    elif contains(game_info.own_cards, Character.Assassin):
+        bot_battle.play_discard_choice(1) # discard Assassin instead of Assassin or Captain
+
+    else:
+        bot_battle.play_discard_choice(0) # discard whatever else instead of Captain
+
+# Gets fresh data on every instance and checks if we need to 
+# perform a move
 if __name__ == "__main__":
     while True:
         game_info = bot_battle.get_game_info()
